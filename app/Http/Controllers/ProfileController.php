89e664312ef9c7dd85e773a3ef241d332fc25b5a
AhmedKhaled <ahmedfarfouroo7@gmail.com>
2024-02-24 11:30:07 +0200

    returning index function & route

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,17 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 
+
 class ProfileController extends Controller
 {
     /**
      * Display the user's profile form.
      */
+    public function index(User $user) {
+        return view('profiles.index', ['user'=>$user]);
+    }
+
     public function edit(Request $request): View
     {
         return view('profile.edit', [
