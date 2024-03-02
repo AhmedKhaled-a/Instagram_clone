8831c3b29ab3c56a881365717d2122c430cd6774
AhmedKhaled221 <ahmedfarfouroo7@gmail.com>
2024-03-02 15:27:52 +0200

    more stylings

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,8 +1,10 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
+        <a href="{{ route('profile.index', ['user' => Auth::user()]) }}" class="text-decoration-none">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('Profile') }}
+            </h2>
+        </a>
     </x-slot>
 
     <div class="py-12">
